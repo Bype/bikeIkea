8aa2415f326b899df2167a11777facbe5dd92ce1
David Olivari <david.olivari@gmail.com>
2015-11-04 06:57:52 +0100

    event loop logging added

--- bike/producer.py
+++ bike/producer.py
@@ -3,20 +3,22 @@ import zmq
 import random
 import sys
 import pygame
+from gelfclient import UdpClient
+import socket
 
-def producer():
-	context = zmq.Context()
-	zmq_socket = context.socket(zmq.PUSH)
-	prod_id = int(sys.argv[1])
-	print "I am consumer #%s" % (prod_id)
-	zmq_socket.connect("tcp://127.0.0.1:5557")
-    # Start your result manager and workers before you start your producers
-	for num in xrange(20000):
-		work_message = { 'id':prod_id,'num' :  random.randrange(0,250)}
-		zmq_socket.send_json(work_message)
-		time.sleep(random.randrange(20,200)/100.0)
 
-#producer()
+class Publisher:
+	def __init__(self):
+		self.gelf = UdpClient('log.bype.org', port=12201, mtu=8000, source=socket.gethostname())
+		context = zmq.Context()
+		self.zmq = context.socket(zmq.PUSH)
+		self.zmq.connect("tcp://127.0.0.1:5557")
+
+	def logPower(self,aPower):
+		self.gelf.log("bike",power=aPower)
+
+	def pushPower(self,aPower):
+		self.zmq.send_json({ 'bike':1,'power' :  aPower})
 
 
 class UIBike:
@@ -24,6 +26,9 @@ class UIBike:
 		self.power = 0
 		self.targety = 0
 		self.icon = []
+		self.currentIcon = 0
+		self.nextIcon = 0
+		self.iconx = 240
 
 	def setupScreen(self):
 		size = width, height = 480,272
@@ -38,13 +43,16 @@ class UIBike:
 		for i in xrange(6):
 			self.icon.append(pygame.image.load("img/%d.png"%(i+1)).convert_alpha())
 		self.screen.blit(self.bg,self.bgrect)
-		pygame.display.update()
-
+		pygame.display.flip()
 
 		
 	def setPower(self,p):
 		self.power = p
 		self.targety = int((self.scalerect.h-272)-(self.power*self.ratio))
+
+	def setIcon(self,i):
+		self.nextIcon = i
+
 
 	def update(self):
 		self.screen.blit(self.bg,self.bgrect)
@@ -54,20 +62,39 @@ class UIBike:
 			self.cropy -= (self.cropy-self.targety)/10
 		self.screen.blit(self.scale,(64,0),(0,self.cropy,self.scalerect.w,self.cropy+272))
 		pygame.display.update((64,0,147,272))
-
-
+		if self.currentIcon != self.nextIcon:
+			if(self.iconx<480):
+				self.screen.blit(self.icon[self.currentIcon],(self.iconx,36),self.icon[self.currentIcon].get_rect())
+				self.iconx += (500-self.iconx)/5
+			else:
+				self.currentIcon = self.nextIcon
+			pygame.display.update((240,0,480,272))
+		else:
+			if(240 < self.iconx):
+				self.screen.blit(self.icon[self.nextIcon],(self.iconx,36),self.icon[self.currentIcon].get_rect())
+				self.iconx -= (self.iconx-240)/5
+			pygame.display.update((240,0,480,272))
+		
 myBike = UIBike()
+myLog = Publisher()
 
+pygame.time.set_timer(pygame.USEREVENT+1, 20000)
+pygame.time.set_timer(pygame.USEREVENT+2, 1000)
+pygame.time.set_timer(pygame.USEREVENT+3, 10000)
 myBike.setupScreen()
 
-c = 0
-
-while myBike.power < 200:
-	myBike.update()
-	time.sleep(0.04)
-	if c%100==0:
-		myBike.setPower(random.randrange(0,200));
-	c = (c+1)%100
+clock = pygame.time.Clock()
 
 while True:
+	for event in pygame.event.get():
+		if event.type == pygame.USEREVENT+1:
+			myLog.logPower(myBike.power)
+		if event.type == pygame.USEREVENT+2:
+			myLog.pushPower(myBike.power)			            
+		if event.type == pygame.USEREVENT+3:
+			myBike.setPower(random.randrange(0,200))
+			myBike.setIcon(random.randrange(1,5))
+		if event.type == pygame.QUIT:
+			break
 	myBike.update()
+	clock.tick(25)
