09c16c23e1cec769f8da3a055a5b3db7ed149e9e
David Olivari <david.olivari@gmail.com>
2015-11-04 06:57:07 +0100

    Objectification Logging added

--- server/consumer.py
+++ server/consumer.py
@@ -2,22 +2,37 @@ import time
 import zmq
 import random
 import pprint
+import socket
+from gelfclient import UdpClient
 
-context = zmq.Context()
-consumer_receiver = context.socket(zmq.PULL)
-consumer_receiver.bind("tcp://127.0.0.1:5557")
 
-result = [0,0,0,0,0,0,0,0]    
-c = 0
+class Consumer:
+	def __init__(self):
+		self.gelf = UdpClient('log.bype.org', port=12201, mtu=8000, source=socket.gethostname())
+		context = zmq.Context()
+		self.zmq = context.socket(zmq.PULL)
+		self.zmq.bind("tcp://0.0.0.0:5557")	
+		self.result = [0,0,0,0,0,0,0,0]    
+
+	def consume(self):
+		data = self.zmq.recv_json()
+		self.result[data['bike']] = data['power']
+		self.sum=0
+		for n in self.result:
+			self.sum += n
+
+	def publish(self):
+		print self.result," ",self.sum
+		self.gelf.log("zone",sum=self.sum,details=self.result.__str__())
+
+
+
+myConsumer = Consumer()
+c=0
 
 while True:
-    data = consumer_receiver.recv_json()
-    result[data['id']] = data['num']
-    s = 0
-    for n in result:
-    	s += n
-    if c == 0:
-    	print "power :",s
-    c = (c + 1) % 20
+	myConsumer.consume()
+	if c == 0:
+		myConsumer.publish()
+	c = (c + 1) % 10
 
-consumer()
