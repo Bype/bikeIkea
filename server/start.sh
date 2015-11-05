849a0d2b6cc973426a822a7c2a3b336964ad2c05
David Olivari <david.olivari@gmail.com>
2015-11-05 06:54:35 +0100

    startup script

---
+++ server/start.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+IP=`/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'`
+HOSTNAME=`cat /etc/hostname`
+MSG=\{\"short_message\":\"startup\",\"host\":\"$HOSTNAME\",\"address\":\"$IP\"\}
+echo $MSG
+curl -XPOST http://gelf.bype.org/gelf -p0 -d $MSG
+
+
