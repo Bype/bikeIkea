6bf35c3a75752d2c12ea4def8f00ca7652eb36ee
David Olivari <david.olivari@gmail.com>
2015-11-18 17:54:05 +0100

    Integrating shield

--- bike/analogReading.py
+++ bike/analogReading.py
@@ -8,24 +8,19 @@ rs = redis.StrictRedis(host='localhost', port=6379, db=0)
 wpi.wiringPiSetup()
 wpi.mcp3004Setup(70,0)
 
-rawcurrent = [0,0,0,0,0,0,0,0,0,0]
 rawvoltage = [0,0,0,0,0,0,0,0,0,0]
 
 i=0
 while True:
-	rawcurrent[i] = (wpi.analogRead(70) *3.3)/1024.0
-	rawvoltage[i] = (wpi.analogRead(73) *3.3)/1024.0
+	rawvoltage[i] = (wpi.analogRead(73) *33)/1024.0
 	i = (i+1)%10
-	sumcurrent =0
 	sumvoltage =0
 	for j in range(10):
-		sumcurrent += rawcurrent[j]
 		sumvoltage += rawvoltage[j]
-	power =  int(math.floor(13 * sumcurrent))
+	power =  int(math.floor(13 * sumvoltage))
 	if 200 < power:
 		power = 200
 	r = rs.set('voltage', sumvoltage)
-	r = rs.set('current', sumcurrent)
 	r = rs.set('power', power)
-	time.sleep(.01)
-
+	print power
+	time.sleep(.1)
