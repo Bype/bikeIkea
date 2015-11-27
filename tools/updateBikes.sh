29b6ac7286ae2cb20da9c5ac2dee8f6ef501a14f
David Olivari <david.olivari@gmail.com>
2015-11-27 17:42:46 +0100

    adding reboot

--- tools/updateBikes.sh
+++ tools/updateBikes.sh
@@ -2,4 +2,5 @@
 
 for i in `/usr/sbin/arp | grep wlan0 | awk '{ print $1 }'`; do 
 	ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no pi@$i git -C /home/pi/bikeIkea pull; 
+	ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no pi@$i sudo shutdown -r +1
 done
